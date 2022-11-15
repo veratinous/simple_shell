71f9b928923e4dfbabbb17be673f295d868578ec
vera magbuwe <veratinous@gmail.com>
2022-11-15 18:58:08 +0100

    just starting

---
+++ getline.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+char *_getline()
+{
+    char *cmd = NULL;
+    size_t n;
+
+    printf(">>> ");
+    if (getline(*cmd, n, stdin) == -1)
+    {
+        free(cmd);
+        exit(0);
+    }
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "shell.h"
+
+int main(void)
+{
+    printf("#");
+    _getline();
+}
